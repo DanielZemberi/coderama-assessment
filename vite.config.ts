5c8dfdb0797ef206e2c40125e7b332c4290b0797
Daniel Zemberi <daniel.zemberi@01people.com>
2023-08-07 16:16:05 +0200

    fix: build fail

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { dependencies } from './package.json';
 
-function renderChunks(deps: Record<string, string>) {
-  const chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
-}
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-router-dom', 'react-dom'],
-          ...renderChunks(dependencies),
-        },
-      },
-    },
-  },
 });
