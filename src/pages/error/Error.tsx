f69592de0509ab98fc69753792581e473b00aeb7
Daniel Zemberi <daniel.zemberi@01people.com>
2023-08-07 12:25:48 +0200

    feat: error page

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import React from 'react';
 
 interface ErrorProps {
@@ -7,10 +8,16 @@ interface ErrorProps {
 
 const Error: React.FC<ErrorProps> = ({ status, errorText }) => {
   return (
-    <div>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="calc(100vh - 220px)"
+    >
       <h1>{status}</h1>
       {errorText ? <p>{errorText}</p> : null}
-    </div>
+    </Box>
   );
 };
 
